cdde45a16369650dfc728d4166bb74a13c7e3cbb
Timothy Stapleton <retrocede@outlook.com>
2021-01-06 18:02:45 -0700

    remove player code. prep for port to use legion ecs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod map;
 mod map_builder;
-mod player;
 mod camera;
 
 pub mod prelude {
@@ -13,7 +12,6 @@ pub mod prelude {
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
     pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
     pub use crate::map::*;
-    pub use crate::player::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
 }
@@ -22,7 +20,6 @@ use prelude::*;
 
 struct State {
     map: Map,
-    player: Player,
     camera: Camera,
 }
 impl State {
@@ -31,7 +28,6 @@ impl State {
         let map_builder = MapBuilder::build(&mut rng);
         Self {
             map: map_builder.map,
-            player: Player::new(map_builder.player_start),
             camera: Camera::new(map_builder.player_start),
         }
     }
@@ -43,9 +39,8 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+        // TODO: execute systems
+        // TODO: Render Draw Buffer
     }
 }
 
